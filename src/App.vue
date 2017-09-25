2551090921a57c761a1d5d833c0a3b79994e70b4
Jesse Goodenough <jesse.goodenough@gmail.com>
2017-09-25 14:12:47 -0400

    resolve merge conflicts

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <SiteNav></SiteNav>
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import SiteNav from './components/SiteNav'
+
+export default {
+  name: 'app',
+  components: {
+    SiteNav
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0;
+}
+</style>
---
+++ src/components/SiteNav.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="site-nav">
+    <ul>
+      <li><a class="site-nav__site-logo" v-bind:href="siteURL">{{ siteName }}</a></li>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sitenav',
+  data () {
+    return {
+      siteName: 'JesseGoodenough',
+      siteURL: 'www.jessegoodenough.com'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+.site-nav {
+  display: block;
+  width: 100%;
+  margin-left: 5%;
+}
+
+.site-nav__site-logo {
+  text-decoration: none;
+  color: #000;
+  padding: 0 .5em;
+  margin: 0;
+  font-weight: bold;
+  display: inline-block;
+}
+
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: inline-block;
+    color: #000;
+    text-align: center;
+    padding: 0 .5em;
+    text-decoration: none;
+}
+
+/* Change the link color to #111 (black) on hover */
+li a:hover {
+    /*background-color: #111;*/
+}
+</style>
