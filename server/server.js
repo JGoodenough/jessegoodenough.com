036849513ad0a9f96a3b3366c8a258e94eda87b7
Jesse Goodenough <jesse.goodenough@gmail.com>
2017-09-27 10:01:37 -0400

    console.log after app.listen crashes app

--- server/server.js
+++ server/server.js
@@ -4,5 +4,6 @@ var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname+'/../dist'));
 var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+app.listen(port, function() {
+    console.log("App is running on port " + port);
+});
