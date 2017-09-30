55abe91ea0ff07456bf61448e78c33f2abcb4a27
Jesse Goodenough <jesse.goodenough@gmail.com>
2017-09-30 17:53:43 -0400

    add in connect history api fallback since this is a SPA for url paths

--- server/server.js
+++ server/server.js
@@ -2,8 +2,12 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
+var history = require('connect-history-api-fallback');
 app.use(serveStatic(__dirname+'/../dist'));
 var port = process.env.PORT || 5000;
+
+app.use(history());
+
 app.listen(port, function() {
     console.log("App is running on port " + port);
 });
