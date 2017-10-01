9c519c5fc4d4edf42358aaf1d5efdf8944690d1d
Jesse Goodenough <jesse.goodenough@gmail.com>
2017-09-30 21:19:40 -0400

    change express middleware order

--- server/server.js
+++ server/server.js
@@ -3,10 +3,12 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 var history = require('connect-history-api-fallback');
-app.use(serveStatic(__dirname+'/../dist'));
-var port = process.env.PORT || 5000;
 
 app.use(history());
+
+app.use(serveStatic(__dirname+'/../dist'));
+
+var port = process.env.PORT || 5000;
 
 app.listen(port, function() {
     console.log("App is running on port " + port);
