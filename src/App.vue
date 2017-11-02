8ae78a09ecad6b09ced6b4c530c930cd349dd542
Jesse Goodenough <jesse.goodenough@gmail.com>
2017-11-02 13:24:27 -0400

    add transition and sliding to main content

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <SiteNav></SiteNav>
-    <transition name="slide">
+    <transition name="slide-fade" appear mode="out-in">
       <router-view></router-view>
     </transition>
   </div>
@@ -37,5 +37,19 @@ body {
   color: $site-color;
   margin-top: 0;
   background-color: $site-background-color;
+}
+
+/* Enter and leave animations can use different */
+/* durations and timing functions.              */
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
 }
 </style>
