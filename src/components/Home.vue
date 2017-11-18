d47131425c634343abd29b83ad809abefd0f4856
Jesse Goodenough <jesse.goodenough@gmail.com>
2017-11-18 11:17:13 -0500

    styling home bg image

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -44,33 +44,35 @@ a {
   position: relative;
   display: flex;
   min-width: 100%;
+  min-height: 50em;
+  height: auto;
 }
 .home__background-site-image {
-  position: absolute;
+  // position: absolute;
   display: flex;
-  min-width: 1024px;
-  height: 1024px;
-  background: url('../assets/images/jesse-tesla-finish.jpeg') no-repeat;
-  -webkit-transform: rotate(90deg);
-  transform: rotate(90deg);
-  background-size: cover;
-  margin-top: 4em auto;
+  // min-width: 1024px;
+  width: 100%;
+  // height: 1024px;
+  background: url('../assets/images/jesse-tesla-finish.png') no-repeat 50% 50%;
+  background-size: contain;
   text-align: center;
   align-content: center;
+  margin: 0 auto;
   @include clearfix;
+
+  height: auto;
+  margin: 0 auto;
+  padding: 10px;
+  position: relative;
 }
 .home__title {
   position: absolute;
   z-index: 1;
   margin: 4em auto;
-  -webkit-transform: rotate(-90deg);
-  transform: rotate(-90deg);
 }
 .home__sub-title {
   position: absolute;
   z-index: 1;
   margin: 4em auto;
-  -webkit-transform: rotate(-90deg);
-  transform: rotate(-90deg);
 }
 </style>
